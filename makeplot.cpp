60608ff3ab5ec249a8e2c1b555e641b8edca482a
kayamash <yamashita@stu.kobe-u.ac.jp>
2018-11-20 14:59:01 +0900

    bug fix

--- makeplot.cpp
+++ makeplot.cpp
@@ -103,7 +103,9 @@ void draw_hist2d(string filename,string graph,Double_t *nbin,string title_axis,s
 
 void draw_samehist2d(string filename,Int_t num,string *graph,Double_t *nbin,string title_axis,string title_png){
 	TCanvas *c1 = new TCanvas("c1","c1",1000,1000);
-	gStyle->SetTitleYOffset(1.0);
+	gStyle->SetTitleYOffset(1.5);
+	gStyle->SetPadLeftMargin(0.165);
+	gStyle->SetPadRightMargin();
 	TH1F *frame1 = gPad->DrawFrame(nbin[0],nbin[1],nbin[2],nbin[3]);
 	frame1->SetTitle(title_axis.c_str());
 	TFile *tf1 = new TFile(filename.c_str());
